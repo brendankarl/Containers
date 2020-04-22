eab1a1ea9134dda980eea3cfb4dd31e97bb7b94b
Brendan Griffin <brendg@microsoft.com>
2020-04-22 11:45:33 +0100

    Commit

---
+++ WorkoutGenerator - Docker Compose/db/init.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE exercises;
+use exercises;
+
+CREATE TABLE exerciselist (
+  exercise VARCHAR(20)
+);
+
+INSERT INTO exerciselist
+  (exercise)
+VALUES
+  ('50 Star Jumps'),
+  ('20 Crunches'),
+  ('30 Squats'),
+  ('30 Sec Arm Circles'),
+  ('30 Sit Ups');
---
+++ WorkoutGenerator - Docker Compose/web/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import mysql.connector
+import json
+
+app = Flask(__name__)
+
+
+def exercises():
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'exercises'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM exerciselist')
+    results = [{exercise} for (exercise) in cursor]
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+@app.route('/')
+def index():
+    return str(exercises())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
